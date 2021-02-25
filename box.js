54d758410cded7e05cd5889e109be4eca2a95061
ToxicSharkORG <73337206+ToxicSharkORG@users.noreply.github.com>
2021-02-25 10:42:25 +0530

    Update box.js

--- box.js
+++ box.js
@@ -2,7 +2,7 @@ class Box{
     constructor(x, y, width, height) {
         var options = {
             'restitution':0.4,
-            'friction':0
+            'friction':0.5
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
